39ff6fc2805e84f04a6ac938a924af345f2ec207
Dave <dave@davedunne.com>
2021-06-03 13:19:03 -0700

    Add Bootstrap formatting and button

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <main className="Container">
-        <h1>Hello</h1>
-        <Quote />
-      </main>
+      <div class="px-4 py-5 my-5 text-center">
+        <h1 class="display-5 fw-bold">Random Quotes</h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">A site that displays a random quote.</p>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <Quote />
+          </div>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" class="btn btn-primary btn-lg px-4 gap-3 m-4">
+              Get Quote
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
